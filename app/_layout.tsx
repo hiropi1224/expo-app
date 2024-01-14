a88a0d67ebc8f44361f926267f0f77390a90274c
hiropi1224 <hiropi1224s@gmail.com>
2024-01-14 16:58:14 +0900

    ✨ 2つの画面を含むスタックレイアウト

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,7 @@
-import { Stack } from "expo-router/stack";
-import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+import { Slot } from "expo-router";
 
 export default function Layout() {
   return (
-    <ThemeProvider value={DarkTheme}>
-      <Stack
-        screenOptions={{
-          title: "Header",
-          headerTintColor: DarkTheme.colors.text,
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
-    </ThemeProvider>
+      <Slot />
   );
 }
